5e7235975dafa47dbb286e387e7cb6af1aca04d1
Yusuke Shiya <yusuke.shiya@gmail.com>
2023-10-28 19:59:58 +0900

    fix: dispose処理を追加

--- lib/widget/new_expense.dart
+++ lib/widget/new_expense.dart
@@ -12,6 +12,13 @@ class _NewExpenseState extends State<NewExpense> {
   final _amountController = TextEditingController();
 
   @override
+  void dispose() {
+    _titleController.dispose();
+    _amountController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16),
